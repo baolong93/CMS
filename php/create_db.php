7cf48e780d80e6d977a295680ba9041f44ff3f8b
Bao Long <baolongngo@eri-167-221.d.port.ac.uk>
2014-04-09 21:42:41 +0100

    create database

--- php/create_db.php
+++ php/create_db.php
@@ -1,50 +1,51 @@
 <?php
 include 'connect.php';
 
+//Comeback to initialize some example product.
 
-if (mysqli_connect_errno())
+
+if (mysqli_connect_errno($mysqli))
   {
-  	echo "<script>console.log('Failed to connect to MySQL: ' . mysqli_connect_error();</script>";
+  	echo "Fail to connect to the database!".$mysqli_connect_error();
   }
 
-//echo "<script>console.log('this is a test');</script>"; writes to console
-
-// Changes to 'shop_db'
-if (mysql_select_db("cms")) {
-	//echo "<script>console.log('connected to shop_db');</script>";
+ // Change the database to cms. 
+if ($mysqli->select_db("cms")) {
  }
 // Create database
 else{
 	$sql = "CREATE DATABASE cms";
-	if (mysql_query($sql))
+	if ($mysqli->query($sql))
   	{
-  		echo "<script>console.log('Database INSE created successfully');</script>";
+  		echo "Database has been created successfully!";
   	}
 	else
   	{
-  		echo "<script>console.log('Error creating database: ' . mysqli_error($con));</script>";
+  		echo "Something went wrong!";
   	}
 }
 
-// create  table
-$table = "product";
-if(mysql_num_rows(mysql_query("SHOW TABLES LIKE '".$table."'"))==1) {}
+// create  Product table.
+$table = "Product";
+if(mysqli_num_rows($mysqli->query("SHOW TABLES LIKE '".$table."'"))==1) {} //Check existance of the table.
 else {
 $query = "CREATE TABLE Product(ID INT NOT NULL AUTO_INCREMENT,
           Name VARCHAR(20) NOT NULL,
           Price INT(5) NOT NULL,
           NumberofProduct INT(5) NOT NULL,
           Description VARCHAR(200) NOT NULL,
-          Picture VARCHAR(20), 
+          Picture VARCHAR(50), 
           PRIMARY KEY (ID));";
 // Execute query
-if (mysql_query($query))
+if ($mysqli->query($query))
   {
   echo "Table user created successfully";
   } else
   {
-  echo "Error creating table: " . mysql_error($connection);
+  echo "Error creating table: " . mysqli_error($mysqli);
   }
 }
+
+
 
 ?>
